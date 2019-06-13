233290f62739c4fd58b5e2f05efc99a17cf8072e
Kimwoochang <nanenchanga53@gmail.com>
2019-06-13 15:08:51 +0900

    4 - 2 submit버튼 동작 추가

--- TriviaApp/app.js
+++ TriviaApp/app.js
@@ -1,51 +1,52 @@
 // initialize variables
-var questionID, question, choiceA, choiceB, choiceC, choiceD, questions, numQuestions, qInfo,
-current = 0;
+var questionID, question, choiceA, choiceB, choiceC, choiceD, questions, numQuestions, qInfo, userChoice
+current = 0,
+score = 0,
+points = [];
 
 var defaultQuestions = [
-{
-    question: "Where are the three smallest bones in the human body?",
-    choiceA: "middle ear",
-    choiceB: "nose",
-    choiceC: "toes",
-    choiceD: "eyes",
-    correct: "A"
-},
-{
-    question: "What is the most abundant element in the Universe?",
-    choiceA: "Helium",
-    choiceB: "Oxygen",
-    choiceC: "Lithium",
-    choiceD: "Hydrogen",
-    correct: "D"
-},
-{
-    question: "Approximately how long does it take for light to travel from the Sun's surface to the Earth?",
-    choiceA: "8 days",
-    choiceB: "8 seconds",
-    choiceC: "8 minutes",
-    choiceD: "8 hours",
-    correct: "C"
-},
-{
-    question: "What is 10/2?",
-    choiceA: "5",
-    choiceB: "2",
-    choiceC: "8",
-    choiceD: "9",
-    correct: "A"
-},
-{
-    question: "Which planet has the most moons?",
-    choiceA: "Saturn",
-    choiceB: "Mars",
-    choiceC: "Jupiter",
-    choiceD: "Uranus",
-    correct: "C"
-}];
+    {
+        question: "Where are the three smallest bones in the human body?",
+        choiceA: "middle ear",
+        choiceB: "nose",
+        choiceC: "toes",
+        choiceD: "eyes",
+        correct: "A"
+    },
+    {
+        question: "What is the most abundant element in the Universe?",
+        choiceA: "Helium",
+        choiceB: "Oxygen",
+        choiceC: "Lithium",
+        choiceD: "Hydrogen",
+        correct: "D"
+    },
+    {
+        question: "Approximately how long does it take for light to travel from the Sun's surface to the Earth?",
+        choiceA: "8 days",
+        choiceB: "8 seconds",
+        choiceC: "8 minutes",
+        choiceD: "8 hours",
+        correct: "C"
+    },
+    {
+        question: "What is 10/2?",
+        choiceA: "5",
+        choiceB: "2",
+        choiceC: "8",
+        choiceD: "9",
+        correct: "A"
+    },
+    {
+        question: "Which planet has the most moons?",
+        choiceA: "Saturn",
+        choiceB: "Mars",
+        choiceC: "Jupiter",
+        choiceD: "Uranus",
+        correct: "C"
+    }];
 
 // reference HTML elements
-var elQuiz = document.getElementById("quiz");
 var elQuizStatus = document.getElementById("quizStatus");
 
 var elQuestion = document.getElementById("question");
@@ -58,6 +59,7 @@ var elChoices = document.getElementsByName('choices');
 // start quiz
 populateQuestions();
 renderQuestion();
+document.getElementById("submit").onclick = gradeQuestion;
 
 function populateQuestions(){
     // populate with default questions
@@ -93,4 +95,37 @@ function renderQuestion(){
     elChoiceB.innerHTML = choiceB;
     elChoiceC.innerHTML = choiceC;
     elChoiceD.innerHTML = choiceD;
+}
+
+function gradeQuestion(){
+    if(getUserChoice()){
+        if(userChoice == questions[current].correct){
+            score++;
+            points[current] = 1;
+        }
+        else{
+            points[current] = 0;
+        }
+        // next question
+        current++;
+        renderQuestion();
+    }
+}
+
+function getUserChoice(){
+    for (var i = 0, length = elChoices.length; i < length; i++)
+    {
+        if (elChoices[i].checked)
+        {
+            userChoice = elChoices[i].value;
+
+            // clear radio input for next question
+            elChoices[i].checked = false;
+
+            return true;
+        }
+    }
+    // user didn't select an answer
+    alert("Please select an answer before continuing");
+    return false;
 }
